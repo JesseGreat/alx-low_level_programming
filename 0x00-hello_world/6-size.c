368811509e5ed176a9736971a0a242316ca96c53
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2022-09-10 03:59:30 -0700

    print standard error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the size of many vars types
+ * main -Prints the size of many var types
  *
  * Return: Always 0 (Success)
  */
@@ -11,10 +11,12 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 printf("Size of a float: %d byte(s)\n", sizeof(e));
-return (0)
+return (0);
 }
+
