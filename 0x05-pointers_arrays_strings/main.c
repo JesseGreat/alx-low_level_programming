5036a817aeef69d9d361f2657c8250d87e5a4cef
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2022-09-20 21:09:41 -0700

    function that swaps the values of two integers

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,3 +1,4 @@
+julien@ubuntu:~/0x05$ cat 0-main.c
 #include "main.h"
 #include <stdio.h>
 
@@ -16,3 +17,35 @@ int main(void)
     printf("n=%d\n", n);
     return (0);
 }
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
+julien@ubuntu:~/0x05$ ./0-98 
+n=402
+n=98
+julien@ubuntu:~/0x05$
+
+julien@ubuntu:~/0x05$ cat 1-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a;
+    int b;
+
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-swap.c -o 1-swap
+julien@ubuntu:~/0x05$ ./1-swap 
+a=98, b=42
+a=42, b=98
+julien@ubuntu:~/0x05$
