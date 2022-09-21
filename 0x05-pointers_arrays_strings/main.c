6690c374af836ab085938d9c3d94f5e45bc8e3a6
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2022-09-20 21:34:23 -0700

    my task alx from 4-9 on pointers array adresses

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -49,3 +49,191 @@ julien@ubuntu:~/0x05$ ./1-swap
 a=98, b=42
 a=42, b=98
 julien@ubuntu:~/0x05$
+
+julien@ubuntu:~/0x05$ cat 2-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strlen.c -o 2-strlen
+julien@ubuntu:~/0x05$ ./2-strlen
+16
+julien@ubuntu:~/0x05$
+
+julien@ubuntu:~/0x05$ cat 3-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-puts.c -o 3-puts
+julien@ubuntu:~/0x05$ ./3-puts
+I do not fear computers. I fear the lack of them - Isaac Asimov
+julien@ubuntu:~/0x05$
+
+
+julien@ubuntu:~/0x05$ cat 4-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-print_rev.c -o 4-print_rev
+julien@ubuntu:~/0x05$ ./4-print_rev
+vomisA caasI - meht fo kcal eht raef I .sretupmoc raef ton od I
+julien@ubuntu:~/0x05$
+
+
+julien@ubuntu:~/0x05$ cat 5-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-rev_string.c -o 5-rev_string
+julien@ubuntu:~/0x05$ ./5-rev_string
+My School
+loohcS yM
+julien@ubuntu:~/0x05$
+
+
+julien@ubuntu:~/0x05$ cat 6-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-puts2.c -o 6-puts2
+julien@ubuntu:~/0x05$ ./6-puts2
+02468
+julien@ubuntu:~/0x05$
+
+
+julien@ubuntu:~/0x05$ cat 7-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts_half(str);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-puts_half.c -o 7-puts_half
+julien@ubuntu:~/0x05$ ./7-puts_half
+56789
+julien@ubuntu:~/0x05$
+
+
+julien@ubuntu:~/0x05$ cat 8-main.c
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[5];
+
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-main.c 8-print_array.c -o 8-print_array
+julien@ubuntu:~/0x05$ ./8-print_array
+98, 402, -198, 298, -1024
+julien@ubuntu:~/0x05$
+
+
+julien@ubuntu:~/0x05$ cat 9-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98];
+    char *ptr;
+
+    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", ptr);
+    return (0);
+}
+julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-main.c 9-strcpy.c -o 9-strcpy
+julien@ubuntu:~/0x05$ ./9-strcpy
+First, solve the problem. Then, write the code
+First, solve the problem. Then, write the code
+julien@ubuntu:~/0x05$ 
